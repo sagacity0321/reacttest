9f781e593f326cb8d1cccaddbcd36f53557250da
sagacity0321 <citronica0321@gmail.com>
2023-09-17 09:55:38 +0900

    popup close & open

--- src/App.js
+++ src/App.js
@@ -4,52 +4,70 @@ import "./App.css";
 import Popup from "./common/Popup";
 
 function App() {
-    const test = "travel";
-    const [list, setList] = useState([
-        "송도센트럴파크",
-        "파주 프로방스 / 헤이리 마을",
-        "춘천 소양강 스카이워크",
-    ]);
+  const test = "travel";
+  const [list, setList] = useState([
+    "송도센트럴파크",
+    "파주 프로방스 / 헤이리 마을",
+    "춘천 소양강 스카이워크",
+  ]);
 
-    const [count, setCount] = useState(0);
-    return (
-        <div className="wrap">
-            <header>
-                <h1>
-                    <span>가자</span>
-                    <span>여행</span>
-                </h1>
-            </header>
-            <main>
-                <div className="textInput">
-                    <input
-                        type="text"
-                        placeholder="가고싶은 여행지를 입력하세요"
-                    />
-                    <button type="button">ADD</button>
-                </div>
-                <div className="listCountName">
-                    Total
-                    <span id="listCountNumber">{count}</span>
-                </div>
-                <ul className="list">
-                    <li>
-                        <p>{list[0]}</p>
-                        <i className="fa-solid fa-trash-can"></i>
-                    </li>
-                    <li>
-                        <p>{list[1]}</p>
-                        <i className="fa-solid fa-trash-can"></i>
-                    </li>
-                    <li>
-                        <p>{list[2]}</p>
-                        <i className="fa-solid fa-trash-can"></i>
-                    </li>
-                </ul>
-            </main>
-            <Popup />
+  const [count, setCount] = useState(0);
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <div className="wrap">
+      <header>
+        <h1>
+          <span>가자</span>
+          <span>여행</span>
+        </h1>
+      </header>
+      <main>
+        <button
+          onClick={() => {
+            setIsOpen(true);
+          }}>
+          열기
+        </button>
+        <button
+          onClick={() => {
+            setIsOpen(false);
+          }}>
+          닫기
+        </button>
+        <button
+          onClick={() => {
+            setIsOpen(!isOpen);
+          }}>
+          toggle
+          {/* 열기 닫기 모두 가능 */}
+        </button>
+        <div className="textInput">
+          <input type="text" placeholder="가고싶은 여행지를 입력하세요" />
+          <button type="button">ADD</button>
         </div>
-    );
+        <div className="listCountName">
+          Total
+          <span id="listCountNumber">{count}</span>
+        </div>
+        <ul className="list">
+          <li>
+            <p>{list[0]}</p>
+            <i className="fa-solid fa-trash-can"></i>
+          </li>
+          <li>
+            <p>{list[1]}</p>
+            <i className="fa-solid fa-trash-can"></i>
+          </li>
+          <li>
+            <p>{list[2]}</p>
+            <i className="fa-solid fa-trash-can"></i>
+          </li>
+        </ul>
+      </main>
+      {isOpen === false ? null : <Popup list={list} setIsOpen={setIsOpen} />}
+    </div>
+  );
 }
 
 export default App;
--- src/common/Popup.js
+++ src/common/Popup.js
@@ -1,17 +1,35 @@
-export default function Popup() {
-    return (
-        <>
-            <section className="popup">모달창</section>
-        </>
-    )
+export default function Popup({ list, setIsOpen }) {
+  return (
+    <>
+      <section className="popup">
+        {list[0]}
+        <button
+          onClick={() => {
+            setIsOpen(false);
+          }}>
+          close
+        </button>
+      </section>
+    </>
+  );
 }
 
 // 컴포넌트를 만들고 App.js에 import해서 사용하는 방법
 /*
-    1. common 폴더를 만들고 그 안에 Popup.js 파일을 만든다.
-    2. Popup.js 파일에 컴포넌트를 만든다.
-    3. App.js 파일에 import해서 사용한다.
-    *import Popup from "./common/Popup";
-    *<Popup />
-    *컴포넌트 이름은 대문자로 시작한다.
+  1. common 폴더를 만들고 그 안에 Popup.js 파일을 만든다.
+  2. Popup.js 파일에 컴포넌트를 만든다.
+  3. App.js 파일에 import해서 사용한다.
+  *import Popup from "./common/Popup";
+  *<Popup />
+  *컴포넌트 이름은 대문자로 시작한다.
+*/
+/*
+// 이런 방식으로도 사용할 수 있다.
+  export default function Popup(props) {
+    return (
+      <>
+        <section className="popup">{props.list[0]}</section>
+      </>
+    )
+  }
 */
